531afaa59b7dda4bc19d3f07b86d21b4c1069ff1
michaelrustam <akbar0711@gmail.com>
2024-08-19 15:02:50 +0500

    git push2

--- user_launch_tmpl.tf
+++ user_launch_tmpl.tf
@@ -3,6 +3,15 @@ resource "aws_launch_template" "team_images_launch_t" {
   image_id      = "ami-0ae8f15ae66fe8cda"
   instance_type = "t2.micro"
 
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              sudo amazon-linux-extras install epel -y
+              sudo yum install stress -y
+              stress --cpu 2 --timeout 30000
+              yum install -y htop
+              EOF
+  )
+
   tag_specifications {
     resource_type = "instance"
     tags = {
@@ -15,6 +24,16 @@ resource "aws_launch_template" "team_videos_launch_t" {
   name_prefix   = "team_videos_launch_t"
   image_id      = "ami-0ae8f15ae66fe8cda"
   instance_type = "t2.micro"
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              sudo amazon-linux-extras install epel -y
+              sudo yum install stress -y
+              stress --cpu 2 --timeout 30000
+              yum install -y htop
+              EOF
+  )
+
   tag_specifications {
     resource_type = "instance"
     tags = {
